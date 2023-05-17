03385949e54e914005536267df613aa549f5565b
Abdulla Al Mamun <hmdabdulla121@gmail.com>
2023-05-17 23:03:58 +0600

    added basic layout setup and create header & footer page

--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Header from '../pages/SharedComponents/Header/Header';
+import { Outlet } from 'react-router-dom';
+import Footer from '../pages/SharedComponents/Footer/Footer';
 
 const Main = () => {
     return (
         <div>
-            <h1>Main page</h1>
+            <Header></Header>
+            <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
