27c89e6fd46c6c5669b550286f4aa0427814119e
Abdulla Al Mamun <hmdabdulla121@gmail.com>
2023-05-19 00:06:29 +0600

    search option dynamically working functionality implemented

--- src/pages/AllToys/AllToys.jsx
+++ src/pages/AllToys/AllToys.jsx
@@ -1,14 +1,13 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { Button, Container, Form, Table } from 'react-bootstrap';
-import {  Link, useLocation, useNavigate } from 'react-router-dom';
-import { AuthContext } from '../../providers/AuthProvider';
+import { Link } from 'react-router-dom';
+
 
 const AllToys = () => {
-    const {user} = useContext(AuthContext);
-    const navigate = useNavigate();
 
-    
+    const [searchText, setSearchText] = useState("");
     const [allToys, setAllToys] = useState([]);
+
     useEffect(() => {
         fetch('http://localhost:5000/allToys?limit=20')
             .then(res => res.json())
@@ -17,51 +16,52 @@ const AllToys = () => {
             })
     }, [])
 
-
+    const handleSearch = () => {
+        fetch(`http://localhost:5000/toySearchByName/${searchText}`)
+            .then(res => res.json())
+            .then(data => {
+                setAllToys(data);
+            })
+    }
     return (
         <div>
             <h3 className='text-center m-4'>ALL TOYS HERE:{allToys.length}</h3>
             <Form className="d-flex w-25 mx-auto mb-3">
-                <Form.Control
-                    type="search"
-                    placeholder="Search"
-                    className="me-2"
-                    aria-label="Search"
-                />
-                <Button variant="outline-success">Search</Button>
+                <input onChange={(e) => setSearchText(e.target.value)} type='text' className='p-1' />
+                <Button onClick={handleSearch} variant="outline-success">Search</Button>
             </Form>
-                <Container>
-                    <Table striped="columns">
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>Seller</th>
-                                <th>Toy Name</th>
-                                <th>Sub-category</th>
-                                <th>Price</th>
-                                <th>Available Quantity</th>
-                                <th>View Details</th>
-                            </tr>
-                        </thead>
-                      
-                        {
-                            allToys.map((toy,index)=><tbody
+            <Container>
+                <Table striped="columns">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Seller</th>
+                            <th>Toy Name</th>
+                            <th>Sub-category</th>
+                            <th>Price</th>
+                            <th>Available Quantity</th>
+                            <th>View Details</th>
+                        </tr>
+                    </thead>
+
+                    {
+                        allToys.map((toy, index) => <tbody
                             key={toy._id}
-                            >
-                                <tr>
-                                    <td>{index + 1}</td>
-                                    <td>{toy.data.sellerName}</td>
-                                    <td>{toy.data.name}</td>
-                                    <td>{toy.data.subCategory}</td>
-                                    <td>{toy.data.price}</td>
-                                    <td>{toy.data.quantity}</td>
-                                    <td><Link className='btn btn-success' to={`/ToyViewDetails/${toy._id}`}>View</Link></td>
-                                </tr>
-                            </tbody>)
-                        }
-                       
-                    </Table>
-                </Container>
+                        >
+                            <tr>
+                                <td>{index + 1}</td>
+                                <td>{toy.data.sellerName}</td>
+                                <td>{toy.data.name}</td>
+                                <td>{toy.data.subCategory}</td>
+                                <td>{toy.data.price}</td>
+                                <td>{toy.data.quantity}</td>
+                                <td><Link className='btn btn-success' to={`/ToyViewDetails/${toy._id}`}>View</Link></td>
+                            </tr>
+                        </tbody>)
+                    }
+
+                </Table>
+            </Container>
         </div>
     );
 };
--- src/pages/SharedComponents/NavigationBar/NavigationBar.jsx
+++ src/pages/SharedComponents/NavigationBar/NavigationBar.jsx
@@ -22,9 +22,9 @@ const NavigationBar = () => {
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="me-auto">
-                            <Link className='text-decoration-none d-flex align-items-center  m-2' to="/home">Home</Link>
-                             <Link className='text-decoration-none d-flex align-items-center  m-2' to="/allToys">All Toys</Link>
-                            
+                            <Link className='text-decoration-none d-flex align-items-center  m-2' to="/">Home</Link>
+                            <Link className='text-decoration-none d-flex align-items-center  m-2' to="/allToys">All Toys</Link>
+
                             {
                                 user && <Link className='text-decoration-none d-flex align-items-center  m-2' to="/myToys">My Toys</Link>
                             }
