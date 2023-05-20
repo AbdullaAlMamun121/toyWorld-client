892b3d1526de5b7340b3b5d4ae832872f5c19eb8
Abdulla Al Mamun <hmdabdulla121@gmail.com>
2023-05-20 18:20:31 +0600

    added all qestion and answer

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -7,63 +7,49 @@ const Blog = () => {
         <div className="col-md-6 mx-auto">
           <div className="card mb-4">
             <div className="card-header">
-              Tell us the differences between uncontrolled and controlled components.
+              What is an access token and refresh token? How do they work and where should we store them on the client-side?
             </div>
             <div className="card-body">
               <p className="card-text">
-                Uncontrolled components:
-                <ul>
-                  <li>Manage their own state and behavior internally</li>
-                  <li>Are self-contained and have no dependencies on other parts of the application</li>
-                  <li>Can be used in various contexts without modification</li>
-                  <li>Useful for simple, standalone functionality</li>
-                </ul>
-                Controlled components:
-                <ul>
-                  <li>Rely on external state management and behavior control</li>
-                  <li>Receive their state and behavior as props from a parent component</li>
-                  <li>Communicate changes back to the parent component through callbacks</li>
-                  <li>More flexible and customizable than uncontrolled components</li>
-                  <li>Suitable for complex forms, data grids, and interactive visualizations</li>
-                </ul>
+                An access token is a credential used to authenticate and authorize a user in a system. It is typically a JSON web token (JWT) that contains encoded information about the user and their permissions. Access tokens have a limited lifespan and are used to access protected resources or perform certain actions on behalf of the user.
+                A refresh token is a long-lived token that is used to obtain a new access token when the current one expires. It is used to maintain user sessions and allows the user to stay logged in without needing to provide their credentials again.
+                Access tokens should be stored on the client-side, typically in the browser's memory or local storage. They should be sent with each request to authenticate the user. Refresh tokens, on the other hand, should be securely stored on the server-side, as they have longer lifespans and need to be protected.
               </p>
             </div>
           </div>
           <div className="card mb-4">
             <div className="card-header">
-              What is a custom hook, and why will you create a custom hook?
+              Compare SQL and NoSQL databases?
             </div>
             <div className="card-body">
               <p className="card-text">
-                A custom hook is a reusable JavaScript function in a React application that allows you to share logic across multiple components. You might create a custom hook to avoid code duplication, abstract complex logic, or encapsulate stateful logic that needs to be shared between multiple components.
+                SQL (Structured Query Language) databases are relational databases that use tables and predefined schemas to store and organize data. They provide strong consistency, ACID (Atomicity, Consistency, Isolation, Durability) transactions, and support complex queries using SQL.
+                NoSQL (Not only SQL) databases, on the other hand, are non-relational databases that store data in flexible, schema-less formats like key-value pairs, documents, graphs, or wide-column stores. They provide high scalability, flexible schemas, and eventual consistency. NoSQL databases are often used for handling large amounts of unstructured or semi-structured data.
               </p>
             </div>
           </div>
           <div className="card mb-4">
             <div className="card-header">
-              How to validate React props using PropTypes?
+              What is Express.js? What is Nest.js?
             </div>
             <div className="card-body">
               <p className="card-text">
-                To validate React props using PropTypes, import PropTypes from the 'prop-types' package, add a 'propTypes' object to your component's definition that specifies the expected types and shapes of the props, use your component and pass in the props, and React will validate that the props match the expected types and shapes. If a prop is missing or has an unexpected type or shape, React will log a warning in the console. Make sure to remove or disable PropTypes checks in production builds.
+
+                Express.js is a minimal and flexible web application framework for Node.js. It provides a simple, yet powerful set of features for building web applications and APIs. Express.js is known for its lightweight nature and its ability to handle middleware, routing, and HTTP request/response handling efficiently.
+
+                Nest.js is a progressive Node.js framework for building scalable and efficient server-side applications. It is built with TypeScript and takes advantage of decorators, dependency injection, and modular architecture. Nest.js is heavily inspired by Angular and provides a structured and opinionated approach to building back-end applications, making it suitable for large-scale projects.
               </p>
             </div>
           </div>
           <div className="card mb-4">
             <div className="card-header">
-              Tell us the difference between Node.js and Express.js.
+              What is MongoDB aggregate and how does it work?
             </div>
             <div className="card-body">
               <p className="card-text">
-                Node.js is a runtime environment for executing JavaScript code outside of a web browser, while Express.js is a framework for building web applications in Node.js.
-                <br /><br />
-                Node.js provides the foundation for building server-side applications in JavaScript, while Express.js builds on top of Node.js by providing an easy-to-use set of features for building web applications.
-                <br /><br />
-                Node.js provides core modules for handling low-level tasks such as file system access and network communication, while Express.js provides higher-level abstractions for tasks such as routing, middleware, and template rendering.
-                <br /><br />
-                Node.js can be used with other web frameworks besides Express.js, while Express.js is specifically designed for building web applications in Node.js.
-                <br /><br />
-                Node.js is more general-purpose and can be used for a variety of applications, while Express.js is focused on building web applications and provides a streamlined set of features for that purpose.
+                MongoDB's aggregate is a powerful aggregation framework that allows for advanced data analysis and transformation operations on collections of documents. It provides a way to perform complex queries, grouping, filtering, and data transformations within the database.
+                The aggregate pipeline in MongoDB consists of multiple stages, where each stage performs a specific operation on the data. Stages can include operations like $match for filtering documents, $group for grouping data, $project for shaping the output, and many more.
+                By combining these stages in a pipeline, developers can define complex data processing logic and perform computations within the database, reducing the amount of data transferred over the network and improving performance
               </p>
             </div>
           </div>
